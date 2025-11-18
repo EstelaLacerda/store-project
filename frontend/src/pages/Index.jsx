b00bf18bd5a8561407d904d30172def9247f996b
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2025-11-18 09:05:28 -0300

    add: begin index page

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,9 +1,25 @@
 import Header from "../components/Header";
+import "./style/Index.css";
 
 export default function Index() {
     return (
-        <>
+        <div className="full-screen-layout">
             <Header />
-        </>
+            <div className="container">
+                <div className="side-a">
+                    <span className="side-title">
+                        Bem vindo de Volta!
+                    </span>
+                    <form className="login-form">
+                        <input type="text" placeholder="Usuário" className="input-field" />
+                        <input type="password" placeholder="Senha" className="input-field" />
+                        <button type="submit" className="submit-button">Entrar</button>
+                    </form>
+                </div>
+                <div className="side-b">
+                    B
+                </div>
+            </div>
+        </div>
     );
 }
